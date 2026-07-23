9220ada729615a91d9ba817eab49dc1ef99c9d53
Ausaf <ausaf@Ausafs-MacBook-Air.local>
2026-07-23 22:22:50 +0530

    fix(reviews): shorten survey delay

--- public/js/google-customer-reviews.js
+++ public/js/google-customer-reviews.js
@@ -16,7 +16,7 @@
 
   function estimatedDeliveryDate() {
     const date = new Date();
-    date.setDate(date.getDate() + 5);
+    date.setDate(date.getDate() + 3);
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
